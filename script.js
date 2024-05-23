d7118614d8dfa0add10b8817db8b7368e5cdd1d3
nevsetakpohano <aya101318@gmail.com>
2024-05-23 14:28:05 +0300

    Made lab 3

---
+++ script.js
@@ -0,0 +1,145 @@
+const canvasNeNapr = document.getElementById("neNapr");
+const canvasNapr = document.getElementById("napr");
+
+const contextNeNapr = canvasNeNapr.getContext("2d");
+const contextNapr = canvasNapr.getContext("2d");
+
+const qntnNodes = 10;
+const coef = 1 - 0 * 0.02 - 1 * 0.005 - 0.25;
+const radius = 15;
+
+const nodePositions = [
+  { x: 120, y: 250 }, //1
+  { x: 200, y: 150 }, //2
+  { x: 300, y: 110 }, //3
+  { x: 400, y: 150 }, //4
+  { x: 480, y: 250 }, //5
+
+  { x: 480, y: 350 }, //6
+  { x: 400, y: 450 }, //7
+  { x: 300, y: 490 }, //8
+  { x: 200, y: 450 }, //9
+  { x: 120, y: 350 }, //10
+];
+
+function generateAdjacencyMatrix() {
+  const seed = 3301;
+  let matrix = [];
+  Math.seedrandom(seed);
+  for (let i = 0; i < qntnNodes; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < qntnNodes; j++) {
+      matrix[i][j] = Math.random() * 2 * coef;
+      matrix[i][j] = matrix[i][j] < 1 ? 0 : 1;
+    }
+  }
+  return matrix;
+}
+
+function drawNodes(context) {
+  nodePositions.forEach((position, index) => {
+    context.fillStyle = "#DAB785";
+    context.beginPath();
+    context.arc(position.x, position.y, radius, 0, Math.PI * 2, true);
+    context.fill();
+    context.stroke();
+    context.font = "14px Times New Roman";
+    context.fillStyle = "black";
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+    context.fillText(`${index + 1}`, position.x, position.y);
+  });
+}
+
+const drawArrow = (x, y, context, angle) => {
+  const arrowSize = 4;
+  context.save();
+  context.translate(x, y);
+  context.rotate(angle);
+  context.moveTo(0, 0);
+  context.lineTo(arrowSize, -arrowSize);
+  context.lineTo(0, 0);
+  context.lineTo(arrowSize, arrowSize);
+  context.closePath();
+  context.restore();
+};
+
+function drawEdges(matrix, context, napr) {
+  context.strokeStyle = "rgba(0, 0, 0, 1)";
+  context.lineWidth = 2;
+  for (let i = 0; i < qntnNodes; i++) {
+    for (let j = 0; j < qntnNodes; j++) {
+      if (matrix[i][j] === 1) {
+        const startX = nodePositions[j].x;
+        const startY = nodePositions[j].y;
+        const endX = nodePositions[i].x;
+        const endY = nodePositions[i].y;
+
+        context.beginPath();
+        context.moveTo(startX, startY);
+        context.lineTo(endX, endY);
+        context.stroke();
+
+        if (context === contextNapr) {
+          const angle = Math.atan2(endY - startY, endX - startX);
+          const indentX = radius * Math.cos(angle);
+          const indentY = radius * Math.sin(angle);
+
+          drawArrow(startX + indentX, startY + indentY, context, angle);
+        }
+
+        if (i === j) {
+          context.beginPath();
+          if (i < 5) {
+            context.arc(
+              endX,
+              endY - radius * 2,
+              radius,
+              -Math.PI / 2,
+              (3 * Math.PI) / 2,
+              true
+            );
+            if (context === contextNapr) {
+              drawArrow(
+                nodePositions[i].x + 5,
+                nodePositions[i].y - 15,
+                context,
+                -Math.PI / 4
+              );
+            }
+
+            context.stroke();
+          } else {
+            context.arc(
+              endX,
+              endY + radius * 2,
+              radius,
+              Math.PI / 2,
+              (-3 * Math.PI) / 2,
+              true
+            );
+            if (context === contextNapr) {
+              drawArrow(
+                nodePositions[i].x + 5,
+                nodePositions[i].y - 15,
+                context,
+                Math.PI / 4
+              );
+            }
+            context.stroke();
+          }
+        }
+        context.stroke();
+      }
+    }
+  }
+}
+
+const matrix = generateAdjacencyMatrix();
+console.log(matrix);
+
+drawEdges(matrix, contextNeNapr);
+drawEdges(matrix, contextNapr);
+
+drawNodes(contextNeNapr);
+drawNodes(contextNapr);
